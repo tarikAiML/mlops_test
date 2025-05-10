3f90acac7e039efd4a09d97d4e8dc128b2020568
tarikAiML <secondtravel2020@gmail.com>
2025-05-10 18:56:34 -0400

    version4

--- pgadmin_config_local.py
+++ pgadmin_config_local.py
@@ -1,1 +1,6 @@
+import os
+
+DATA_DIR = "/var/lib/pgadmin"
+LOG_FILE = os.path.join(DATA_DIR, "pgadmin4.log")
+SQLITE_PATH = os.path.join(DATA_DIR, "pgadmin4.db")
 SESSION_DB_PATH = '/tmp/pgadmin_sessions'
